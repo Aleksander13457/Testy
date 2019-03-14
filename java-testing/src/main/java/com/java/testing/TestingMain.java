59de6f2482c0e88f8ba8581cf7ab42ef4ac03198
nasz_login_do_githubAleksander13457 <aleksander13457@gmail.com>
2019-03-14 13:15:19 +0100

    Testowanie oprogramowania

---
+++ java-testing/src/main/java/com/java/testing/TestingMain.java
@@ -0,0 +1,7 @@
+package com.java.testing;
+
+public class TestingMain {
+    public static void main(String[] args){
+        System.out.println("Wprowadzenie to testowania oprogramowania");
+    }
+}
---
+++ src/main/java/com/java/JavaKursApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaKursApplication.class, args);
+    }
+
+}
